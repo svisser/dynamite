33420cb53816fca5f260f80973cdcef562c733dd
Simeon Visser <simeon87@gmail.com>
2015-06-02 00:06:12 +0100

    Guard against failure to create socket

--- tools/test.py
+++ tools/test.py
@@ -8,14 +8,16 @@ from dynamite.message import Message
 
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = None
     try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(("127.0.0.1", 8888))
         message = Message()
         s.send(message.to_bytes())
     finally:
-        s.shutdown(socket.SHUT_RDWR)
-        s.close()
+        if s is not None:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
 
 
 if __name__ == '__main__':
